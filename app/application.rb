00344696c57b36b4ee654f60c0d73eaba25e2d0e
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-05 22:42:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      binding.pry
+    #  binding.pry
       if item != nil
         resp.write item.price
       else
