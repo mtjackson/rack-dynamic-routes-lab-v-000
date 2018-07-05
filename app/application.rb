686aece2a559b2701e5443129947c65300145222
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-05 22:34:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,7 @@ class Application
         resp.write "#{item_name.price}"
       else
         resp.write "Item not found"
+        resp.status = 400
       end
 
       resp.write "Route not found"
