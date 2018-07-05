5a06579c358182822aca96bf64d13a4dc25430e6
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-05 22:34:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       if @@items.find {|i| i.name == item_name}
         resp.write "#{item_name.price}"
+      else
       end
 
       resp.write "Route not found"
