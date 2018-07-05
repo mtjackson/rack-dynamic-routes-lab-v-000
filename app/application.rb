f4278dc20b835523a2e827ad0bdee8cd3cf94e91
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-05 22:31:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,8 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.find(item_name)
+      @@items.find(item_name)
+
       resp.write "Route not found"
 
       resp.status = 404
