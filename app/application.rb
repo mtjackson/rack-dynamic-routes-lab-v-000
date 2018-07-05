84634cc363e8d565462e34324945447f450c6a48
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-05 22:30:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/"")
-      @@items.find()
+      item_name = req.path.split("/items/")
+      @@items.find(item_name)
       resp.write "Route not found"
 
       resp.status = 404
